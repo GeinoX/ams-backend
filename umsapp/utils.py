03c16791d539ba79c5ae118c9fe5db84ab979726
GEINOTECH <geinotech@gmail.com>
2026-06-15 10:34:07 +0100

    Refactored project by creating modular Django apps each for a domain entity

--- umsapp/utils.py
+++
@@ -1,9 +0,0 @@
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
-
-def notify_session(session_id, data):
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        f"session_{session_id}",
-        {"type": "session_update", "data": data}
-    )
